e5a26300eb736d107f5c5f44e6c49a1d4374b9ba
RobertoNesta <robertonesta1993@gmail.com>
2023-04-11 15:41:20 +0200

    making the email generator using the for cycle

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,8 +4,20 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        emailGenerator: 10,
+        url: 'https://flynn.boolean.careers/exercises/api/random/mail',
+        mails: []
       }
+    },
+    mounted() {
+        for (let i = 0; i < this.emailGenerator; i++) {
+            axios
+            .get(this.url)
+            .then(response = {
+                
+            })
+        }
+
     }
   }).mount('#app')
 
